4680a82715dbee993889a1a8742f1f9322affa61
Sruesh <abc@gmail.com>
2020-06-11 14:45:31 +0530

    Task Completed - Add customer, Disable button when text box is an empty and display customer name in ngFor loop

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,9 +6,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
-  customers : string[] ;
-
+  customers: string[];
+  customerName: string;
   ngOnInit() {
+    /// Init cutomers value to empty
+    this.customers = [];
+  }
+
+/**
+ * Submit button - Save text box value
+ * @param value Customer Name
+ */
+  onSubmit(value: string): void {
+    this.customers.push(value);
+    this.customerName = '';
   }
 
 }
